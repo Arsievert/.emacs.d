92e2d377a52574525d47fe869965797f2be77821
Austin Sievert <Asievert1@gmail.com>
2021-10-08 20:18:14 -0500

    Add config.el and custom.el

---
+++ config.el
@@ -0,0 +1,47 @@
+;;; config.el    -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 Austin Sievert
+
+;; Author: Austin Sievert <Arsievert1@gmail.com>
+;; URL: https://github.com/Arsievert1/.emacs.d
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Settings provided to quickly change the behavior and look of Emacs.
+
+;;; Code:
+
+;; Terminal-Theme:
+;; Selected color scheme using Terminal Emacs.
+(setq terminal-theme 'modus-vivendi)
+
+;; Terminal-Use-Theme-Background:
+;; 't - use background color as specified by selected terminal theme.
+;; 'nil - use background color as specified by terminal window.
+(setq terminal-use-theme-background 'nil)
+
+;; GUI-Theme:
+;; Selected color scheme when using GUI Emacs.
+(setq gui-theme 'doom-solarized-dark)
+
+;; Font:
+;; Faces used when using GUI Emacs.
+(setq font "Fantasque Sans Mono")
+
+;; Whitespace-Cleanup:
+;; 't - remove trailing whitespace automatically.
+;; 'nil - do not remove trailing whitespace.
+(setq whitespace-cleanup 't)
---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e2acbf379aa541e07373395b977a99c878c30f20c3761aac23e9223345526bcc"
+     "428754d8f3ed6449c1078ed5b4335f4949dc2ad54ed9de43c56ea9b803375c23" default)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ `(default ((t (:family ,font :foundry "outline" :slant normal :weight normal :height 105 :width normal)))))
